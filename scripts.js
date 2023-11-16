bd23d684a73b58068cef15f73e0d1825dd55c0d7
abedarad76 <abedarad76@gmail.com>
2023-11-16 14:47:07 +0330

    validateEmail

--- scripts.js
+++ scripts.js
@@ -43,10 +43,19 @@ function handleSubmit() {
     tableRow.appendChild(tableData);
   });
 
-  Object.keys(submit).forEach((key)=>{
-    document.querySelector("#"+key).value=null;
-  })
+  Object.keys(submit).forEach((key) => {
+    document.querySelector("#" + key).value = null;
+  });
 
   const table = document.querySelector("#student-table");
   table.appendChild(tableRow);
+}
+
+function validateEmail(event) {
+  const regexp = /^[a-zA-Z]{3,}@[a-zA-Z]{2,}.[a-zA-Z]{2,}$/;
+  if (regexp.test(event.target.value)) {
+    document.getElementById("p").innerHTML = "eror";
+  }else{
+    document.getElementById("p").innerHTML = "amir EMl"
+  }
 }
